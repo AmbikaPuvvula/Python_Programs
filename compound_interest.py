9a1d8c9373afc154005d6706da2cbd266090936e
AmbikaPuvvula <45551971+AmbikaPuvvula@users.noreply.github.com>
2019-04-17 12:33:53 +0530

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -4,4 +4,4 @@ def compound_interest(principle, time, rate):
 	CI = principle * (pow((1 + rate / 100), time)) 
 	print("Compound interest is", CI) 
  
-compound_interest(10000, 5, 10.25) 
+compound_interest(15500, 6, 12.25) 
