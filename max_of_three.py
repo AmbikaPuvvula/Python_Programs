7896989a3752fe778fe59dca6242a4cbccc37026
AmbikaPuvvula <45551971+AmbikaPuvvula@users.noreply.github.com>
2019-04-17 12:18:47 +0530

    Update max_of_three.py

--- max_of_three.py
+++ max_of_three.py
@@ -1,7 +1,7 @@
 # Finding Maximum of three numbers
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
 
 if (num1 > num2) and (num1 > num3):
     print("Maximum number is: ", num1)
