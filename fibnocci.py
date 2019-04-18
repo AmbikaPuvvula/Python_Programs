fb22c277fc79c9dd72ca5846d586c5733d2fba6c
AmbikaPuvvula <45551971+AmbikaPuvvula@users.noreply.github.com>
2019-04-18 12:09:25 +0530

    Update fibnocci.py

--- fibnocci.py
+++ fibnocci.py
@@ -1,6 +1,6 @@
-fn=int(input("Enter the first number of the series "))
-sn=int(input("Enter the second number of the series "))
-n=int(input("Enter the number of terms needed "))
+fn=int(input("Enter the first number of the series: "))
+sn=int(input("Enter the second number of the series: "))
+n=int(input("Enter the number of terms needed: "))
 print(fn,sn,end=" ")
 while(n-2):
     tn=fn+sn
